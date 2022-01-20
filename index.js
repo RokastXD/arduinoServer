f82c159a2ddb92db8b4dcf5b869a3e69ebce53e8
Rokast <mr.timurka27@bk.ru>
2022-01-20 17:38:29 +0300

    servak

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
-var http = require('http')
 const app = express()
+const port = 3000
 
-http.createServer(app).listen(80)
+app.post('/', (req, res) => {
+    res.send("It's me, your site")
+    console.log(res);
+})
 
-app.set('title', 'My Site')
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+})
+
